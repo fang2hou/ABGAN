89b146b24b1943232d8d737c2425a65ba5ac752f
Zhou Fang <fang2hou@outlook.com>
2019-11-19 13:33:45 +0900

    update results

--- data/convert_to_levels.py
+++ data/convert_to_levels.py
@@ -19,13 +19,15 @@ convert_time = time.time()
 
 num_converted = 0
 for index, data in enumerate(data_list, 3):
-    ap.data_to_level(dirpath+'/'+data, 'generated_levels/level-{0:2d}.xml'.format(index))
+    for test_threshold in range(1, 10, 1):
+       ap.data_to_level(dirpath+'/'+data, 'generated_levels/level-{0:2d}-{1:.2f}.xml'.format(index, test_threshold*0.1), threshold=test_threshold*0.1)
+
     num_converted += 1
     
     progress = num_converted / num_data * 100
-    stdout.write("\rProgress: %{0:.1f} %%".format(progress))
+    stdout.write("\rProgress: {0:.1f} %".format(progress))
     stdout.flush()
 
 convert_time = time.time() - convert_time
 
-stdout.write("\nDone! Converted {0} data in {1:.3f}s.".format(num_data, convert_time))
+stdout.write("\nDone! Converted {0} data in {1:.3f}s.\n".format(num_data, convert_time))
