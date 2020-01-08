766981e327b14d6c5264101aa7c481826715ba68
Zhou Fang <fang2hou@outlook.com>
2020-01-08 15:59:27 +0900

    add DAnet model

---
+++ abganlibs/models/DAnet.py
@@ -0,0 +1,288 @@
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+
+
+class Self_Attn(nn.Module):
+    """ Self attention Layer"""
+
+    def __init__(self, in_dim, activation):
+        super(Self_Attn, self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+
+        self.query_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.key_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.value_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention value + input feature 
+                attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize, C, width, height = x.size()
+        proj_query = self.query_conv(x).view(
+            m_batchsize, -1, width*height).permute(0, 2, 1)  # B X CX(N)
+        proj_key = self.key_conv(x).view(
+            m_batchsize, -1, width*height)  # B X C x (*W*H)
+        energy = torch.bmm(proj_query, proj_key)  # transpose check
+        attention = self.softmax(energy)  # BX (N) X (N)
+        proj_value = self.value_conv(x).view(
+            m_batchsize, -1, width*height)  # B X C X N
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, width, height)
+
+        out = self.gamma*out + x
+        return out  # , attention
+
+
+class CAM_Module(nn.Module):
+    """ Channel attention module"""
+    def __init__(self, in_dim):
+        super(CAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.softmax  = nn.Softmax(dim=-1)
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X C X C
+        """
+        m_batchsize, C, height, width = x.size()
+        proj_query = x.view(m_batchsize, C, -1)
+        proj_key = x.view(m_batchsize, C, -1).permute(0, 2, 1)
+        energy = torch.bmm(proj_query, proj_key)
+        energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy
+        attention = self.softmax(energy_new)
+        proj_value = x.view(m_batchsize, C, -1)
+
+        out = torch.bmm(attention, proj_value)
+        out = out.view(m_batchsize, C, height, width)
+
+        out = self.gamma*out + x
+        return out
+
+
+class DCGAN_D(nn.Module):
+    def __init__(self, isize, nz, nc, ndf, ngpu, n_extra_layers=0):
+        super(DCGAN_D, self).__init__()
+        self.ngpu = ngpu
+        assert isize % 16 == 0, "isize has to be a multiple of 16"
+
+        main = nn.Sequential()
+        # input is nc x isize x isize
+        main.add_module('initial_conv_{0}-{1}'.format(nc, ndf),
+                        nn.Conv2d(nc, ndf, 4, 2, 1, bias=False))
+        main.add_module('initial_relu_{0}'.format(ndf),
+                        nn.LeakyReLU(0.2, inplace=True))
+        csize, cndf = isize / 2, ndf
+
+        # Extra layers
+        for t in range(n_extra_layers):
+            main.add_module('extra-layers-{0}_{1}_conv'.format(t, cndf),
+                            nn.Conv2d(cndf, cndf, 3, 1, 1, bias=False))
+            # main.add_module('extra-layers-{0}_{1}_batchnorm'.format(t, cndf),
+            #                nn.BatchNorm2d(cndf))
+            main.add_module('extra-layers-{0}_{1}_relu'.format(t, cndf),
+                            nn.LeakyReLU(0.2, inplace=True))
+
+        while csize > 4:
+            in_feat = cndf
+            out_feat = cndf * 2
+            main.add_module('pyramid_{0}_{1}_conv'.format(in_feat, out_feat),
+                            nn.Conv2d(in_feat, out_feat, 4, 2, 1, bias=False))
+            # main.add_module('pyramid_{0}_batchnorm'.format(out_feat),
+            #                nn.BatchNorm2d(out_feat))
+            main.add_module('pyramid_{0}_relu'.format(out_feat),
+                            nn.LeakyReLU(0.2, inplace=True))
+            cndf = cndf * 2
+            csize = csize / 2
+
+        main.add_module('attn', Self_Attn(512, 'relu'))
+
+        # state size. K x 4 x 4
+        main.add_module('final_{0}-{1}_conv'.format(cndf, 1),
+                        nn.Conv2d(cndf, 1, 4, 1, 0, bias=False))
+        self.main = main
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(
+                self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+
+        output = output.mean(0)
+        return output.view(1)
+
+
+class DCGAN_G(nn.Module):
+    def __init__(self, isize, nz, nc, ngf, ngpu, n_extra_layers=0):
+        super(DCGAN_G, self).__init__()
+        self.ngpu = ngpu
+        assert isize % 16 == 0, "isize has to be a multiple of 16"
+
+        cngf, tisize = ngf // 2, 4
+        while tisize != isize:
+            cngf = cngf * 2
+            tisize = tisize * 2
+
+        main = nn.Sequential()
+        # input is Z, going into a convolution
+        main.add_module('initial_{0}-{1}_convt'.format(nz, cngf),
+                        nn.ConvTranspose2d(nz, cngf, 4, 1, 0, bias=False))
+        main.add_module('initial_{0}_batchnorm'.format(cngf),
+                        nn.BatchNorm2d(cngf))
+        main.add_module('initial_{0}_relu'.format(cngf),
+                        nn.ReLU(True))
+
+        csize, cndf = 4, cngf
+        while csize < isize // 2:
+            main.add_module('pyramid_{0}-{1}_convt'.format(cngf, cngf // 2),
+                            nn.ConvTranspose2d(cngf, cngf // 2, 4, 2, 1, bias=False))
+            main.add_module('pyramid_{0}_batchnorm'.format(cngf // 2),
+                            nn.BatchNorm2d(cngf // 2))
+            main.add_module('pyramid_{0}_relu'.format(cngf // 2),
+                            nn.ReLU(True))
+            cngf = cngf // 2
+            csize = csize * 2
+
+        # Extra layers
+        for t in range(n_extra_layers):
+            main.add_module('extra-layers-{0}_{1}_conv'.format(t, cngf),
+                            nn.Conv2d(cngf, cngf, 3, 1, 1, bias=False))
+            main.add_module('extra-layers-{0}_{1}_batchnorm'.format(t, cngf),
+                            nn.BatchNorm2d(cngf))
+            main.add_module('extra-layers-{0}_{1}_relu'.format(t, cngf),
+                            nn.ReLU(True))
+
+        main.add_module('attn', Self_Attn(32, 'relu'))
+
+        main.add_module('final_{0}-{1}_convt'.format(cngf, nc),
+                        nn.ConvTranspose2d(cngf, nc, 4, 2, 1, bias=False))
+        main.add_module('final_{0}_tanh'.format(nc),
+                        nn.ReLU())  # nn.Softmax(1))    #Was TANH nn.Tanh())#
+        self.main = main
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(
+                self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+
+        #print (output[0,:,0,0])
+        # exit()
+        return output
+###############################################################################
+
+
+class DCGAN_D_nobn(nn.Module):
+    def __init__(self, isize, nz, nc, ndf, ngpu, n_extra_layers=0):
+        super(DCGAN_D_nobn, self).__init__()
+        self.ngpu = ngpu
+        assert isize % 16 == 0, "isize has to be a multiple of 16"
+
+        main = nn.Sequential()
+        # input is nc x isize x isize
+        # input is nc x isize x isize
+        main.add_module('initial_conv_{0}-{1}'.format(nc, ndf),
+                        nn.Conv2d(nc, ndf, 4, 2, 1, bias=False))
+        main.add_module('initial_relu_{0}'.format(ndf),
+                        nn.LeakyReLU(0.2, inplace=True))
+        csize, cndf = isize / 2, ndf
+
+        # Extra layers
+        for t in range(n_extra_layers):
+            main.add_module('extra-layers-{0}_{1}_conv'.format(t, cndf),
+                            nn.Conv2d(cndf, cndf, 3, 1, 1, bias=False))
+            main.add_module('extra-layers-{0}_{1}_relu'.format(t, cndf),
+                            nn.LeakyReLU(0.2, inplace=True))
+
+        while csize > 4:
+            in_feat = cndf
+            out_feat = cndf * 2
+            main.add_module('pyramid_{0}-{1}_conv'.format(in_feat, out_feat),
+                            nn.Conv2d(in_feat, out_feat, 4, 2, 1, bias=False))
+            main.add_module('pyramid_{0}_relu'.format(out_feat),
+                            nn.LeakyReLU(0.2, inplace=True))
+            cndf = cndf * 2
+            csize = csize / 2
+
+        # state size. K x 4 x 4
+        main.add_module('final_{0}-{1}_conv'.format(cndf, 1),
+                        nn.Conv2d(cndf, 1, 4, 1, 0, bias=False))
+        self.main = main
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(
+                self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+
+        output = output.mean(0)
+        return output.view(1)
+
+
+class DCGAN_G_nobn(nn.Module):
+    def __init__(self, isize, nz, nc, ngf, ngpu, n_extra_layers=0):
+        super(DCGAN_G_nobn, self).__init__()
+        self.ngpu = ngpu
+        assert isize % 16 == 0, "isize has to be a multiple of 16"
+
+        cngf, tisize = ngf // 2, 4
+        while tisize != isize:
+            cngf = cngf * 2
+            tisize = tisize * 2
+
+        main = nn.Sequential()
+        main.add_module('initial_{0}-{1}_convt'.format(nz, cngf),
+                        nn.ConvTranspose2d(nz, cngf, 4, 1, 0, bias=False))
+        main.add_module('initial_{0}_relu'.format(cngf),
+                        nn.ReLU(True))
+
+        csize, cndf = 4, cngf
+        while csize < isize // 2:
+            main.add_module('pyramid_{0}-{1}_convt'.format(cngf, cngf // 2),
+                            nn.ConvTranspose2d(cngf, cngf // 2, 4, 2, 1, bias=False))
+            main.add_module('pyramid_{0}_relu'.format(cngf // 2),
+                            nn.ReLU(True))
+            cngf = cngf // 2
+            csize = csize * 2
+
+        # Extra layers
+        for t in range(n_extra_layers):
+            main.add_module('extra-layers-{0}_{1}_conv'.format(t, cngf),
+                            nn.Conv2d(cngf, cngf, 3, 1, 1, bias=False))
+            main.add_module('extra-layers-{0}_{1}_relu'.format(t, cngf),
+                            nn.ReLU(True))
+
+        main.add_module('final_{0}-{1}_convt'.format(cngf, nc),
+                        nn.ConvTranspose2d(cngf, nc, 4, 2, 1, bias=False))
+        main.add_module('final_{0}_tanh'.format(nc),
+                        nn.Softmax())  # Tanh())
+        self.main = main
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(
+                self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+        return output
