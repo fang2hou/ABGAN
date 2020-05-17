a898bdc1c0f753201667c94413a103c164582d6e
Zhou Fang <fang2hou@outlook.com>
2020-05-17 17:49:26 +0900

    change train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-#call activate fang
+call activate fang
 export PYTHONPATH=abganlibs:$PYTHONPATH
 python experiments/wgan_gp_3.py --cuda --ngpu=1 
 #python tools/original_level_generator/generator_competition.py
